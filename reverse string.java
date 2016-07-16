ad5f07dc8d6f3af92b4e049eed57dc1586bf2b58
Maheswarinatarajan <maheswari1021@gmail.com>
2016-07-16 15:44:47 +0530

    Add files via upload

---
+++ reverse string.java
@@ -0,0 +1,11 @@
+  public static void main(String[] args) {
+        System.out.println("enter the string");
+        Scanner sc=new Scanner(System.in);
+        String s1="maheswari";
+        String s2=new StringBuffer(s1).reverse().toString();
+        System.out.println(""+s2);
+        
+     
+        // TODO code application logic here
+    }
+}
