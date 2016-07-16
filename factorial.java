7ef400bae3df1436517393678fcee1029bf01292
Maheswarinatarajan <maheswari1021@gmail.com>
2016-07-16 17:08:38 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+System.out.println("enter the no");
+        Scanner sc=new Scanner(System.in);
+        int i,fact=1;
+        int num=sc.nextInt();
+        for(i=1;i<=num;i++)
+        {
+        fact=fact*i;
+        
+    }
+       
+        System.out.println(""+fact);
+        // TODO code application logic here
+    }
+}
