0f0f84cfa7c5bbdfcbc071838e00874c8c68f687
Maheswarinatarajan <maheswari1021@gmail.com>
2016-07-16 14:55:24 +0530

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,20 @@
+  public static void main(String[] args) {
+        System.out.println("enter the letter");
+        Scanner sc=new Scanner(System.in);
+        char ch;
+        ch=sc.next().charAt(0);
+        if(ch=='a'|| ch=='A'|| ch=='e'|| ch=='E'|| ch=='i'|| ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U')
+        {
+            System.out.println("vowel");
+        }
+        else{
+            
+            System.out.println("consonants");
+        }
+           
+        
+        }
+        
+        
+        
+        // TODO cod
