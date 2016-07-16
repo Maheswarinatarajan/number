c2239f1589b57e822058a4068c05b505e353b7da
Maheswarinatarajan <maheswari1021@gmail.com>
2016-07-16 12:36:49 +0530

    Add files via upload

---
+++ fine.java
@@ -0,0 +1,12 @@
+ public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+         int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("even");
+            
+        }
+        else{
+            System.out.println("odd");
+        }
+        
+        // TODO code application logic he
