7c25903547fb277eb07c514dad4cfb536d3c446b
Maheswarinatarajan <maheswari1021@gmail.com>
2016-08-06 17:13:36 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,19 @@
+
+
+    public class ConstructorDemo {
+     
+     int a,b;
+
+    ConstructorDemo (int x, int y){
+     
+    a=x;
+    b=y;
+     
+    }
+    public static void main(String[] args) {
+     
+            ConstructorDemo ob= new ConstructorDemo ();
+     
+        }
+    }
+
