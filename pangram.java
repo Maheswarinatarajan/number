535afb34065ced7331b39e2ec4bcb0736ec815e1
Maheswarinatarajan <maheswari1021@gmail.com>
2016-08-07 10:22:42 +0530

    Add files via upload

---
+++ pangram.java
@@ -0,0 +1,28 @@
+package com.javafries.string.pangram;
+ 
+public class PangramChecker {
+ 
+    private static final int NO_OF_LETTERS_OF_ALPHABET = 26;
+ 
+    public static void main(String[] args) {
+        String sentence = "Freight to me sixty dozen quart jars and twelve "
+                + "black pans.";
+        System.out.println("Is pangram : " + isPangram(sentence));
+    }
+ 
+    private static boolean isPangram(String sentence) {
+ 
+        if (sentence.length() < NO_OF_LETTERS_OF_ALPHABET) {
+            return false;
+        }
+ 
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            if (sentence.indexOf(ch) < 0
+                    && sentence.indexOf((char) (ch + 32)) < 0) {
+                return false;
+            }
+        }
+ 
+        return true;
+    }
+}
